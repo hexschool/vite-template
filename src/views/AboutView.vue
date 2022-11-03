1c390e1cbabb73081af1dcd87aa1b9707dc7405c
yinmin <alice0050722@gmail.com>
2022-11-03 11:58:48 +0800

    fix: eslint error 修改檔案名稱

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,6 @@
 
 <script>
 export default {
-  name: 'AboutPage'
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,6 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomePage',
   components: {
     HelloWorld
   }
