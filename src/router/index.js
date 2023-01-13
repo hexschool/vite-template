c887f11c090f03061df7cf4fcc57494ad9ccd0b4
yinmin <alice0050722@gmail.com>
2023-01-13 11:23:12 +0800

    fix: 更新 vite 版本

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
