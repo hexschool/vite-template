bc296510dab12103aebbcef31889c2a34c3b110c
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-01-16 10:23:55 +0800

    feat: 🎸 調整樣板
    調整版本以及新增一些功能以便部署

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+import 'dotenv/config';
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import eslintPlugin from 'vite-plugin-eslint';
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
   plugins: [
     vue(),
     eslintPlugin({
