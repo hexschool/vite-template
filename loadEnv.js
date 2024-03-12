4511c47a1c97c32ef09724439880f0c873a85f16
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-03-12 10:49:23 +0800

    feat: 🎸 expansion

---
+++ loadEnv.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+
+const getCurrentTimeString = () => {
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const ampm = hours >= 12 ? '下午' : '上午';
+  hours %= 12;
+  hours = hours || 12;
+  const hoursStr = String(hours).padStart(2, '0');
+  return `${ampm}${hoursStr}:${minutes}:${seconds}`;
+};
+
+const customLog = (tag, message, tagColor = '\x1b[36m', messageColor = '\x1b[32m') => {
+  const timeString = getCurrentTimeString();
+  // eslint-disable-next-line no-console
+  console.log(`\x1b[90m%s\x1b[0m ${tagColor}\x1b[1m%s\x1b[0m ${messageColor}%s\x1b[0m`, timeString, `[${tag}]`, message);
+};
+
+export default (mode) => {
+  const basePath = path.resolve(process.cwd(), '.env');
+  const envPath = `${basePath}.${mode}`;
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+  const envConfig = dotenv.config({ path: finalPath });
+
+  if (envConfig.error) {
+    customLog('vite', '無法讀取到環境變數(Environment Variables)檔案。');
+    customLog('vite', '請在終端機輸入「cp .env.example .env」 建立環境變數檔案。');
+    customLog('vite', '目前僅支援 .env、.env.development、.env.production 三種模式。');
+  }
+};
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -6,39 +5,43 @@ import vue from '@vitejs/plugin-vue';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import eslintPlugin from 'vite-plugin-eslint';
+import loadEnv from './loadEnv';
 
 // eslint-disable-next-line no-control-regex
 const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
-  plugins: [
-    vue(),
-    eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  loadEnv(mode);
+  return ({
+    base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+    plugins: [
+      vue(),
+      eslintPlugin({
+        include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
+    build: {
+      rollupOptions: {
+        output: {
         // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-        sanitizeFileName(name) {
-          const match = DRIVE_LETTER_REGEX.exec(name);
-          const driveLetter = match ? match[0] : '';
-          // substr 是被淘汰語法，因此要改 slice
-          return (
-            driveLetter +
-            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
-          );
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : '';
+            // substr 是被淘汰語法，因此要改 slice
+            return (
+              driveLetter
+            + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            );
+          },
         },
       },
     },
-  },
+  });
 });
